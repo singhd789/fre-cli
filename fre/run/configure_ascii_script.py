06834b3b9bff7d86581d7d1eabea4fcfae65c65a
Dana Singh <Dana.Singh@ldt-4742397.gfdl.noaa.gov>
2026-05-07 14:00:13 -0400

    #877 start confiure-ascii dev

---
+++ fre/run/configure_ascii_script.py
@@ -0,0 +1,10 @@
+"""
+"""
+#imports
+import logging
+fre_logger = logging.getLogger(__name__)
+
+def configure_ascii_subtool():
+    """
+    """
+    #
--- fre/run/frerun.py
+++ fre/run/frerun.py
@@ -4,6 +4,7 @@ entry point for fre run subcommands
 
 import click
 from .frerunexample import run_test_function
+from fre.run import configure_ascii_script
 
 @click.group(help=click.style(" - run subcommands !!!NotImplemented!!!", fg=(164,29,132)))
 def run_cli():
@@ -15,3 +16,12 @@ def function(uppercase):
     """ - Execute fre run test """
     run_test_function(uppercase)
     raise NotImplementedError('fre run has not been implemented yet!')
+
+@run_cli.command()
+@click.option('--uppercase', '-u',
+              is_flag=True,
+              help = "")
+def configure_ascii():
+    """
+    """
+    configure_ascii_script.configure_ascii_subtool()
