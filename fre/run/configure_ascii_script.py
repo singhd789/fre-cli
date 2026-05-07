a9583b4b6c5d3682a0668e2a21201db10c2b7f11
Dana Singh <Dana.Singh@ldt-4742397.gfdl.noaa.gov>
2026-05-07 14:07:32 -0400

    #877 test issue linking

--- fre/run/configure_ascii_script.py
+++ fre/run/configure_ascii_script.py
@@ -4,7 +4,8 @@
 import logging
 fre_logger = logging.getLogger(__name__)
 
-def configure_ascii_subtool():
+def configure_ascii_subtool(output):
     """
     """
-    #
+    # input yaml? (experiment name, directories?, fre version?)
+    # need to write to file
