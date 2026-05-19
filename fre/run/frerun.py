caa6e3b222457caf3011a418b1c44fba406875a8
Dana Singh <Dana.Singh@ldt-4742397.gfdl.noaa.gov>
2026-05-19 15:27:43 -0400

    #889 Start subtool dev for fre run gen-srun

--- fre/run/frerun.py
+++ fre/run/frerun.py
@@ -5,13 +5,23 @@ entry point for fre run subcommands
 import click
 from .frerunexample import run_test_function
 
-@click.group(help=click.style(" - run subcommands !!!NotImplemented!!!", fg=(164,29,132)))
+@click.group(help=click.style(" - run subcommands", fg=(164,29,132)))
 def run_cli():
     ''' entry point to fre run click commands '''
 
 @run_cli.command()
-@click.option('--uppercase', '-u', is_flag=True, help = 'Print statement in uppercase.')
-def function(uppercase):
-    """ - Execute fre run test """
-    run_test_function(uppercase)
-    raise NotImplementedError('fre run has not been implemented yet!')
+@click.option('-y',
+              '--yamlfile',
+              help = "")
+@click.option('-e',
+              '--experiment',
+              multiple = True,
+              help = "")
+@click.option('-s',
+              '--submit',
+              is_flag=True,
+              help = "")
+def gen_srun(yamlfile, experiment, submit):
+    """
+    """
+    generate_srun_script.generate_srun(yamlfile, experiment, submit)
---
+++ fre/run/generate_srun_script.py
@@ -0,0 +1,44 @@
+import 
+
+class Gen_SRUN() 
+    def __init__(yamlfile, experiment, submit):
+        """
+        Initialize the object.
+        """
+        self.yaml = yamlfile
+        self.exp = experiment #(there can be multiple)
+        self.submit = submit
+
+    def load_yaml(self):
+        """
+        Load the resolved yamlfile.
+        """
+
+    def get_layout_info(yaml):
+        """
+        Save the layout information associated with the experiment
+        name passed.
+        """
+
+    def compose_srun_cmd(layout_yaml_dict):
+        """
+        Generate the srun command and print.
+        """
+
+    def run_srun():
+        """
+        Run the srun command if -s, --submit is passed
+        """
+
+## Actual fre-cli subtool call
+def gen_srun()
+    """
+    """
+    # initialize object
+    srun_obj = Gen_SRUN(yamlfile, experiment, submit)
+    yaml_dict = srun_obj.load_yaml()
+    layout_info = srun_obj.get_layout_info(yaml_dict)
+    srun_cmd = srun_obj.compose_srun_cmd(layout_info)
+
+    fre_logger.info("Generated srun commmand: %s", srun_cmd)
+    fre_logger.info("To run the srun command, pass -s or --submit")
