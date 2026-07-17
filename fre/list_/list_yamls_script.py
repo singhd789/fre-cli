c5ad41763ad64900ae640e1e5bf03358b2618f52
Dana Singh <Dana.Singh@ldt-4742397.gfdl.noaa.gov>
2026-07-17 14:01:27 -0400

    #871 update fre list yamls
    - return a space separated string to pass to fre yamltools combine (uw tools)
    - update comments
    - provide warning for yamls that don't exist

--- fre/list_/list_yamls_script.py
+++ fre/list_/list_yamls_script.py
@@ -1,35 +1,41 @@
 """
+List_yamls_script provides methods to list and return YAML configurations to be combined into
+one, resolved configuration. Different click options are avaiable to return only relevant yamls
+depending on the process the user wants to run (compile, runtime, postprocess, analysis).
 """
 import logging
-import yaml
-import pprint
 from pathlib import Path
+import yaml
 
 fre_logger = logging.getLogger(__name__)
 
-def list_yamls_subtool(yamlfile: str, experiment: str, compile_only: bool, runtime_only: bool, postprocess_only: bool, analysis_only: bool):
+def list_yamls_subtool(yamlfile: str, experiment: str, compile_only: bool, runtime_only: bool,
+                       postprocess_only: bool, analysis_only: bool):
     """
-    List the relevant yamls to combine. The default list returned is ALL yamls.
-    (model, compile, platforms, settings, runtime, postprocessing, analysis yamls)
+    List_yamls_subtool lists the relevant yamls to combine. The default list returned is ALL yamls
+    associated with an experiment. (model, compile, platforms, settings, runtime, postprocessing,
+    analysis yamls)
 
-    :param yamlfile: Path to the model yaml configuration file
+    :param yamlfile: is the path to the model yaml configuration file
     :type yamlfile: str
-    :param experiment: Name of the experiment
+    :param experiment: is the name of the experiment
     :type experiment: str
-    :param compile_only: Flag to only pass yaml configs needed for compilation
+    :param compile_only: is the flag to pass yaml configurations needed for compilation
     :type compile_only: boolean 
-    :param runtime_only: Flag to only pass yaml configs needed for model runtime
+    :param runtime_only: is the flag to pass yaml configurations needed for model runtime
     :type runtime_only: boolean 
-    :param postprocess_only: Flag to only pass yaml configs needed for postprocessing
+    :param postprocess_only: is the flag to pass yaml configurations needed for postprocessing
     :type postprocess_only: boolean 
-    :param analysis_only: Flag to only pass yaml configs needed for postprocessing
-    :type analysis_only: boolean 
+    :param analysis_only: is the flag to pass yaml configurations needed for postprocessing
+    :type analysis_only: boolean
+    :return: Space separated string of absolute paths to yaml configurations
+    :rtype: str
     """
     model_yaml = Path(yamlfile).name
     model_yaml_path = Path(yamlfile).resolve().parent
 
     exp_name = experiment
-    with open(yamlfile, 'r') as yf:
+    with open(yamlfile, 'r', encoding="utf-8") as yf:
         yaml_dict = yaml.load(yf, Loader = yaml.Loader)
 
     compile_data = yaml_dict["build"].get("compileYaml").split("/")[-1]
@@ -67,17 +73,26 @@ def list_yamls_subtool(yamlfile: str, experiment: str, compile_only: bool, runti
         fre_logger.info("No experiment name passed. Will only provide YAMLs related to compilation.")
         yamls.extend([compile_data, platform_data])
 
+    yamls_full_path = ""
     # Add full path for yaml configurations
-    yamls_full_path = [f"{str(model_yaml_path)}/"+y for y in yamls]
+    for y in yamls:
+        yamls_full_path += f"{str(model_yaml_path)}/{y} "
 
     # set logger level to INFO
     former_log_level = fre_logger.level
     fre_logger.setLevel(logging.INFO)
 
     fre_logger.info("YAMLS to be combined (Experiment => %s):", experiment)
-    for y in yamls_full_path:
+    for y in yamls_full_path.split():
         fre_logger.info("  - %s", y)
 
     fre_logger.setLevel(former_log_level)
+
+    # Check if the paths exist; give warning
+    fre_logger.warning("")
+    for y in yamls_full_path.split():
+        if not Path(y).exists():
+            fre_logger.warning("**DNE**: %s", y)
+    fre_logger.warning("")
 
     return yamls_full_path
