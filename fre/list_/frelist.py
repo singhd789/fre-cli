28e3dc9ee543fe46ee81306c3b7fee91ee61c8e6
Dana Singh <Dana.Singh@ldt-4742397.gfdl.noaa.gov>
2026-07-21 11:36:57 -0400

    #871 fix help messages

--- fre/list_/frelist.py
+++ fre/list_/frelist.py
@@ -62,27 +62,27 @@ def pp_components(yamlfile, experiment):
               type=bool,
               is_flag=True,
               default = False,
-              help="List yaml configuration files needed for compilation, defined in
-                    the `[model].yaml`. This includes the model, platform, and compile
-                    configuration files.")
+              help="List yaml configuration files needed for compilation, defined in "
+                    "the `[model].yaml`. This includes the model, platform, and compile "
+                    "configuration files.")
 @click.option("--runtime-only",
               type=bool,
               is_flag=True,
               default = False,
-              help="List yaml configuration files needed for model runtime, defined in
-                    the `[model].yaml`.")
+              help="List yaml configuration files needed for model runtime, defined in "
+                    "the `[model].yaml`.")
 @click.option("--postprocess-only",
               type=bool,
               is_flag=True,
               default = False,
-              help="List yaml configuration files needed for postprocessing, defined in
-                    the `[model].yaml`.")
+              help="List yaml configuration files needed for postprocessing, defined in "
+                    "the `[model].yaml`.")
 @click.option("--analysis-only",
               type=bool,
               is_flag=True,
               default = False,
-              help="List yaml configuration files needed for analysis, defined in
-                    the `[model].yaml`.")
+              help="List yaml configuration files needed for analysis, defined in "
+                    "the `[model].yaml`.")
 def yamls(yamlfile, experiment, compile_only, runtime_only, postprocess_only, analysis_only):
     """ - List yamls to be combined"""
     list_yamls_script.list_yamls_subtool(yamlfile, experiment, compile_only, runtime_only, postprocess_only, analysis_only)
